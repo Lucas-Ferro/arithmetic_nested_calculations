7645aff5c7421342a41018e35f5bc8d4e1626353
Lucas Ferro Antunes de Oliveira <48101851+LucasFerroHAILab@users.noreply.github.com>
2022-05-17 15:55:50 -0300

    Add Macros, Delays and Main code for output

---
+++ main.asm
@@ -0,0 +1,37 @@
+.org 0x00
+	rjmp start
+
+.include "minha_lib.inc"
+.include "macros.inc"
+.include "delays.inc"
+
+start:
+	ldi R20, 8 ;inteiro
+	ldi R21, 2 ;decimal
+	mostra_resultado_display
+
+	ldi R20, 6 ;inteiro
+	ldi R21, 5 ;decimal
+	mostra_resultado_botao
+
+	ldi R20, 12 ;inteiro
+	ldi R21, 3 ;decimal
+	ldi R30, 1
+	mostra_resultado_temp
+
+	final_cod:
+		rjmp final_cod
+
+tabela:
+	.db	0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F
+
+pega_tabela:
+	push	R16
+		ldi		ZL, LOW(tabela<<1)
+		ldi		ZH, HIGH(tabela<<1)
+		add		ZL,R16
+		ldi		R16,0
+		adc		ZH,R16
+		lpm		R17,Z
+	pop		R16
+	ret
